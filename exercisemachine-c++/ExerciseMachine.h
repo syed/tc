fe2b607b9fd75107d6ec43ad0b97e3fc9c3120d0
Syed <syed1.mushtaq@gmail.com>
2012-04-14 13:20:54 +0530

    Rectagular grid SRM 145 div2 lv2

--- exercisemachine-c++/ExerciseMachine.h
+++ exercisemachine-c++/ExerciseMachine.h
@@ -12,12 +12,12 @@ using namespace std;
 #define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
 #define ISEQ(c) (c).begin(), (c).end()
 
-/* It can also be solved as GCD(sec,100). Gcd of a,b means a number that can divide both evenly.
+/* It can also be solved as GCD(sec,100)-1. Gcd of a,b means a number that can divide both evenly.
  * now we have number of seconds and 100 as the percentage. at gcd both will divide evenly.
  * So it is a perfect percentage. We can get the pecentage as 100/gcd. every time we have
- * to increase the percentage by 100/gcd amount. So we want to find out for 100 how many
- * times do we have to do this (100/(100/gcd)) = gcd . Now remove first case where we
- * put 0% so we have gcd-1
+ * to increase the percentage by 100/gcd amount for every sec/gcd seconds.
+ * So we want to find out for 100 how many times do we have to do this (100/(100/gcd)) = gcd .
+ * Now remove  case where we put 100% so we have gcd-1
  */
 class ExerciseMachine {
 
